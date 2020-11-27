0604541ae1f9d8ba2f966f0bee07d22f573fb9ef
ehrtnfl20 <ehrtnfl20@kakao.com>
2020-11-27 12:23:20 +0900

    ReactNative SQLite 초기작업

---
+++ RnDataBaseApp/apps/App.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.body}>
+      <Text children=" Hello " style={styles.title} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: '#000000',
+  },
+});
+
+export default App;
