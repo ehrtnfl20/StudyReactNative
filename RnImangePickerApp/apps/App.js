9e56702323f8a36fb96f7d25dd16cedb083457bf
ehrtnfl20 <ehrtnfl20@kakao.com>
2020-12-02 15:09:00 +0900

    앱 icon 이미지 변경

--- RnImangePickerApp/apps/App.js
+++ RnImangePickerApp/apps/App.js
@@ -14,22 +14,39 @@ export default class App extends Component {
 
   showPicker = () => {
     const options = {
-      title: '이미지선택',
+      title: '이미지 선택',
       takePhotoButtonTitle: '카메라',
       chooseFromLibraryButtonTitle: '저장소',
       cancelButtonTitle: '취소',
       // customButtons: [
       //   {name: 'kb', title: '카카오톡 이미지 선택'},
       // ],
+      storageOptions: {
+        skipBackup: true, // iOS / android 무시
+        path: 'Pictures/images/',
+        privateDirectory: true,
+      },
     };
-    ImagePicker.showImagePicker(options, () => {});
+    ImagePicker.showImagePicker(options, (res) => {
+      if (res.didCancel) {
+        alert('취소하였습니다.');
+      } else if (res.error) {
+        alert(`에러 : ${res.error}`);
+      } else if (res.customButton) {
+        //..
+      } else {
+        // 선택된 이미지 경로 가져오기
+        const uri = {uri: res.uri}; //uri이 > url보다 상위 개념
+        this.setState({img: uri});
+      }
+    });
   };
 
   render() {
     return (
       <View style={styles.body}>
         <Button title="이미지 선택" onPress={this.showPicker} />
-        <Text children={this.state.img.uri} style={styles.urlText} />
+        <Text children={this.state.img.uri} style={styles.uriText} />
         <Image source={this.state.img} style={styles.image} />
       </View>
     );
