2bb274967d35ac5c969ab34d0a0bf2467d10b25c
ehrtnfl20 <ehrtnfl20@kakao.com>
2020-12-03 11:49:56 +0900

    화면구현 시뮬레이션

--- RnGoogleMapApp/apps/App.js
+++ RnGoogleMapApp/apps/App.js
@@ -1,10 +1,20 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import {View, Text} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 export default function App() {
   return (
-    <View>
-      <Text children="Hello GoogleMap" />
+    <View style={{flex: 1}}>
+      <MapView style={{flex: 1, margin: 5}}
+         provider={PROVIDER_GOOGLE}
+         initialRegion={{
+          latitude: 35.093112,
+          longitude: 129.126615,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+        }}
+      />
     </View>
   );
 }
